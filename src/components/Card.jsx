8e0908a3bb5e582436bad1aeb1fd23f08fc1fb82
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2022-01-02 08:38:22 +0200

    Accesability; fix breaking points on homepage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,23 @@
 import ButtonComponent from './ButtonComponent'
+import { Link } from 'react-router-dom'
 
 function Card({ item }) {
   console.log(item)
-  const { name, brewery_type, city, state } = item
+  const { name, brewery_type, city, country ,state, website_url } = item
   return (
-    <div className='  bg-gray-200 p-6 font-extralight   shadow-md rounded-md hover:transition duration-700 ease-in-out'>
-      <h1>Name: {name}</h1> <br />
-      <p>brewery Type: {brewery_type}</p> <br />
+    <div className='p-5 mt-4 text-clip bg-gray-200 rounded-md shadow-md font-extralight '>
+      <a class='external' rel='noreferrer' href={website_url} target='_blank'>
+        <h1 className='text-center underline font-bold uppercase'>{name}</h1>{' '}
+        <br />
+      </a>
+      <p>Brewery Type: {brewery_type}</p> <br />
       <p>
-        City : {city}, {state}
+        City : {city}, {state}, {country}
       </p>
       <br />
+      {/* <Link to='/dashboard'> */}
       <ButtonComponent label='More Details' color='primary' />
+      {/* </Link> */}
     </div>
   )
 }
