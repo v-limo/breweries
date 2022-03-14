9d4f13b11eae50e6c868ce2c57731154c4e96a77
Limo <48591371+v-limo@users.noreply.github.com>
2022-03-14 11:34:34 +0200

    Update Header.js, remove nav not yet implemented

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,16 @@ function Header() {
       <div className='mt-2 font-serif text-3xl text-blue-900 uppercase duration-150 cursor-pointerhover:overline hover:underline overline accent-indigo-300 underline-offset-8'>
         <Link to='/'>Brewery</Link>
       </div>
-      <div className='flex justify-between align-baseline'>
+<!--       <div className='flex justify-between align-baseline'>
         <div className='p-4 uppercase cursor-pointer hover:overline'>
           <Link to='/map'>Map</Link>
-        </div>
-        <div className='p-4 uppercase cursor-pointer hover:overline'>
+        </div> -->
+<!--         <div className='p-4 uppercase cursor-pointer hover:overline'>
           <Link to='/pagination'>pagination</Link>
-        </div>
-        <div className='p-4 uppercase cursor-pointer hover:overline'>
+        </div> -->
+<!--         <div className='p-4 uppercase cursor-pointer hover:overline'>
           <Link to='/about'>About</Link>
-        </div>
+        </div> -->
       </div>
     </div>
   )
